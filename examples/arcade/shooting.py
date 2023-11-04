a4f3d148aab145ba2df8fc3d99f282821138b544
Shital Shah <shitals@microsoft.com>
2023-11-04 15:46:30 -0700

    keyboard working

---
+++ examples/arcade/shooting.py
@@ -0,0 +1,20 @@
+from pygamejr import game
+
+game.start()
+
+ball = game.create_sprite("ball.gif", 100, 100)
+
+def ball_keyboard(ball, keys):
+    if "left" in keys:
+        ball.rect = ball.rect.move(-2, 0)
+    elif "right" in keys:
+        ball.rect = ball.rect.move(2, 0)
+    elif "up" in keys:
+        ball.rect = ball.rect.move(0, -2)
+    elif "down" in keys:
+        ball.rect = ball.rect.move(0, 2)
+
+ball.attach_on_keyboard(ball_keyboard)
+
+while True:
+    game.rest()
--- pygamejr/game.py
+++ pygamejr/game.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Set
 
 import pygame
 
@@ -11,11 +11,56 @@ actors = []
 width, height = 320, 240 #1280, 720 #640, 480 #320, 240
 color = "purple"
 fps = 60
+down_keys = set()
 
 class Actor:
     def __init__(self, image_path:str, x:int, y:int):
         self.image = pygame.image.load(utils.full_path_abs(image_path))
         self.rect = self.image.get_rect()
+
+    def on_keyboard(self, keys:Set[str]):
+        pass
+
+    def attach_on_keyboard(self, callback):
+        self.on_keyboard = callback.__get__(self)
+
+    def on_keydown(self, key:str):
+        pass
+
+    def attach_on_keydown(self, callback):
+        self.on_keydown = callback.__get__(self)
+
+    def on_keyup(self, key:str):
+        pass
+
+    def attach_on_keyup(self, callback):
+        self.on_keyup = callback.__get__(self)
+
+    def on_mousedown(self, pos:Tuple[int, int]):
+        pass
+
+    def attach_on_mousedown(self, callback):
+        self.on_mousedown = callback.__get__(self)
+
+    def on_mouseup(self, pos:Tuple[int, int]):
+        pass
+
+    def attach_on_mouseup(self, callback):
+        self.on_mouseup = callback.__get__(self)
+
+    def on_mousemove(self, pos:Tuple[int, int]):
+        pass
+
+    def attach_on_mousemove(self, callback):
+        self.on_mousemove = callback.__get__(self)
+
+    def on_mousewheel(self, pos:Tuple[int, int], y:int):
+        pass
+
+    def attach_on_mousewheel(self, callback):
+        self.on_mousewheel = callback.__get__(self)
+
+
 
 def create_sprite(image_path:str, x:int, y:int) -> Actor:
     actor = Actor(image_path, x, y)
@@ -47,6 +92,32 @@ def rest():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end()
+        if event.type == pygame.KEYDOWN:
+            key_name = pygame.key.name(event.key)
+            down_keys.add(key_name)
+            for actor in actors:
+                actor.on_keydown(key_name)
+        if event.type == pygame.KEYUP:
+            key_name = pygame.key.name(event.key)
+            down_keys.remove(key_name)
+            for actor in actors:
+                actor.on_keyup(key_name)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for actor in actors:
+                actor.on_mousedown(event.pos)
+        if event.type == pygame.MOUSEBUTTONUP:
+            for actor in actors:
+                actor.on_mouseup(event.pos)
+        if event.type == pygame.MOUSEMOTION:
+            for actor in actors:
+                actor.on_mousemove(event.pos)
+        if event.type == pygame.MOUSEWHEEL:
+            for actor in actors:
+                actor.on_mousewheel(event.pos, event.y)
+    if down_keys:
+        for actor in actors:
+            actor.on_keyboard(down_keys)
+
 
     assert screen is not None, "screen is None"
 
