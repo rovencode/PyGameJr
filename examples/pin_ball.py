9fe2dddab077345a771f38b11c7a8cb716e1d358
Shital Shah <shitals@microsoft.com>
2024-02-04 09:13:22 -0800

    pin ball with goodies

--- examples/pin_ball.py
+++ examples/pin_ball.py
@@ -3,31 +3,39 @@ import math
 from pygamejr import game, Vec2d, DrawOptions, common
 import pymunk
 
-#game.show_mouse_coordinates=True
+game.show_mouse_coordinates=True
 #game.set_camera_controls(True)
 game.start(screen_width=600, screen_height=600,
            screen_color="purple", gravity=-900, physics_fps_multiplier=5, screen_fps=50)
 
+ball_radius = 25
+gap_width = ball_radius * 10
+right_flipper_center = (300+gap_width/2, 100)
+left_flipper_center = (300-gap_width/2, 100)
 
-game.create_line((150, 100), (50, 550), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
-game.create_line((450, 100), (550, 550), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
+flipper_points = [(46.7, 40.0), (-93.3, 20.0), (46.7, 0.0)]
+
+game.create_line(left_flipper_center, (50, 550), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
+game.create_line(right_flipper_center, (550, 550), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
 game.create_line((50, 550), (300, 600), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
 game.create_line((300, 600), (550, 550), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
 game.create_line(Vec2d(300, 600-180), (400, 400), color="yellow", elasticity=0.7, fixed_object=True, colliision_group=1)
 
+bar = game.create_rect(width=10, height=50, color="magenta", center=(300, 350), mass=1, elasticity=0.7, colliision_group=1,
+                                 draw_options=DrawOptions(center_radius=3), friction=0.9)
+game.create_pin_joint(bar, actor2=bar.position)
+
+ball1 = game.create_circle(15, color="violet", center=(100, 500), mass=1, elasticity=0.95, colliision_group=1, fixed_object=True)
+ball2 = game.create_circle(15, color="violet", center=(100, 450), mass=1, elasticity=0.95, colliision_group=1)
+game.create_spring_joint(ball1, actor2=ball2, stiffness=0.9, damping=0.2, rest_length=1.0, params_as_ratio=True)
 
 # create flippers
-flipper_points = [Vec2d(20, -20), Vec2d(-120, 0), Vec2d(20, 20)]
-centeroid = sum(flipper_points, Vec2d(0, 0)) / len(flipper_points)
-flipper_points = [v - centeroid for v in flipper_points]
-max_y = max(v.y for v in flipper_points)
-flipper_points = [(v.x, max_y-v.y) for v in flipper_points]
 right_flipper = game.create_polygon_any(flipper_points, color="green",
-                                        center=(450, 100), mass=100, colliision_group=1,
+                                        center=right_flipper_center, mass=100, colliision_group=1,
                                         elasticity=0.4, draw_options=DrawOptions(angle_line_width=1, center_radius=3))
 flipper_points = [(-v.x, v.y) for v in right_flipper.shape.get_vertices()]
 left_flipper = game.create_polygon_any(flipper_points, color="green",
-                                        center=(150, 100), mass=100, colliision_group=1,
+                                        center=left_flipper_center, mass=100, colliision_group=1,
                                         moment=right_flipper.moment,
                                         elasticity=0.4, draw_options=DrawOptions(angle_line_width=1, center_radius=3))
 
@@ -61,15 +69,16 @@ while game.is_running():
     keys = game.key_pressed()
     if 'space' in keys:
         if current_ball is None:
-            current_ball = game.create_circle(25, color="red",
+            current_ball = game.create_circle(ball_radius, color="red",
                 center=(random.randint(115, 350), 400),
                 mass=1, elasticity=0.95)
 
     # repostion the flippers
-    right_flipper.position = Vec2d(450, 100)
-    left_flipper.position = Vec2d(150, 100)
+    right_flipper.position = Vec2d(*right_flipper_center)
+    left_flipper.position = Vec2d(*left_flipper_center)
     right_flipper.velocity = Vec2d(0, 0)
     left_flipper.velocity = Vec2d(0, 0)
+    bar.angular_velocity = min(bar.angular_velocity*0.99, 3600)
 
     # remove balls that are off the screen
     if current_ball is not None and current_ball.position.y < 0:
