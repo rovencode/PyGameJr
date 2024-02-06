4cc1a6c0b6f75c24297aa9165cc4ac7bec4afec5
Shital Shah <shitals@microsoft.com>
2024-02-05 18:05:08 -0800

    fix ball gravity demo

--- examples/ball_gravity.py
+++ examples/ball_gravity.py
@@ -1,10 +1,10 @@
 from pygamejr import game
 
-game.start(gravity=-100)
+game.start(gravity=-900)
 
-game.create_screen_walls(bottom=True)
+game.create_screen_walls(bottom=True, friction=0.0, elasticity=1)
 
-ball1 = game.create_image("ball.gif", (100, 600), density=1)
+ball1 = game.create_image("ball.gif", (100, 600), density=10, elasticity=1, friction=0.0)
 
 ball2 = game.create_circle(radius=30, image_path="ball.gif", center=(400, 600),
                            density=1, angular_velocity=5, elasticity=0.8)
